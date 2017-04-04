c4c9e3a54917ff3b9ecedf95528f124fe8e91fc1
Tomaslaz <tomas.lazauskas@gmail.com>
2017-04-04 10:41:20 +0100

    Adding distribution analysis

--- source/GrandC.py
+++ source/GrandC.py
@@ -10,49 +10,10 @@ import numpy as np
 import sys
 
 import Constants
+import DistributionAnalysis
 import Utilities
 from Utilities import log
-
-def avg_m_analysis(temperatures, chem_pot_range, chem_pot_multi, Wm_array, _accuracy, options):
-  """
-  Calculates and plots average m
   
-  """
-
-  success = True
-  error = ""
-  
-  print "temperatures: ", temperatures
-  print "chem_pot_range: ", chem_pot_range
-  print "chem_pot_multi: ", chem_pot_multi
-  
-  return success, error
-  
-def calc_permutation(m, mm, _accuracy):
-  """
-  Evaluates the permutation expression
-  
-  """
-  
-  cat_sites = 108
-  an_sites = 216
-  
-  up_fac = cat_sites - 2*m
-  down_fac = cat_sites - 2*mm
-  
-  value = Utilities.factorial_devision(up_fac, down_fac, _accuracy)
-  
-  value *= Utilities.factorial_devision(2*m, 2*mm, _accuracy)
-  
-  up_fac = an_sites - m
-  down_fac = an_sites - mm
-  
-  value *= Utilities.factorial_devision(up_fac, down_fac, _accuracy)
-  
-  value *= Utilities.factorial_devision(m, mm, _accuracy)
-  
-  return value
-
 def perform_grand_canonical_analysis(names, permutations, chem_pot_multi, data, _accuracy, options):
   """
   The main method of the grand canonical analysis
@@ -94,16 +55,10 @@ def perform_grand_canonical_analysis(names, permutations, chem_pot_multi, data, 
   
   # Perform chemical potential analysis
   if chem_pot_analysis:
-    # Preparing Wm probabilities    
-    Wm_array = prepare_Wm(chem_pot_multi, temperatures, chem_pot_range, min_energies, delta_E_sums, 
-                          experiment_cnts, _accuracy, options)
-    
-    # Average m analysis
-    success, error = avg_m_analysis(temperatures, chem_pot_range, chem_pot_multi, Wm_array, 
-                                    _accuracy, options)
-#     success, error = analyse_avg_m(temperatures, chem_pot_range, min_energies, delta_E_sums, 
-#                                    names, permutations, chem_pot_multi)
-  
+    # performs distribution analysis    
+    success, error = DistributionAnalysis.distribution_analysis(chem_pot_multi, temperatures, chem_pot_range, 
+                                           min_energies, delta_E_sums, experiment_cnts, _accuracy, options)
+      
   if not success:
     return success, error
   
@@ -173,60 +128,4 @@ def prepare_energies(input_data_array, _accuracy, options):
       
   return energies, min_energies, shifted_energies, experiment_cnts
 
-def prepare_Wm(chem_pot_multi, temperatures, chem_pot_range, min_energies, delta_E_sums, experiment_cnts, 
-               _accuracy, options):
-  """
-  Evaluates Wm with respect to temperature and chemical potential
-  
-  """
-  
-  log(__name__, "Preparing Wm", options.verbose, indent=3)
-  
-  temp_len = len(temperatures)
-  chem_pot_len = len(chem_pot_range)
-  chem_pot_multi_len = len(chem_pot_multi)
-  
-  Wm_array = np.empty([chem_pot_multi_len, temp_len, chem_pot_len], _accuracy)
-  
-  for m_idx in range(chem_pot_multi_len):
-    m_value = chem_pot_multi[m_idx]
-    Emin_m = min_energies[m_idx]
-    
-    # Estimating the top part of Wm
-    sum_top = delta_E_sums[m_idx]
-    
-    for t_i in range(temp_len):
-      kT = np.longdouble(Constants.kB * temperatures[t_i])
-     
-      mu_i = 0
-      for mu in chem_pot_range:    
-        
-        Wm_value = _accuracy(0.0)
-        
-        # Estimating the bottom part of Wm
-        sum_botton = _accuracy(0.0)
-        
-        for mm_index in range(chem_pot_multi_len):
-          mm_value = chem_pot_multi[mm_index]
-          Emin_mm = min_energies[mm_index]
-          
-          # exponential term
-          exp_expr_pow = _accuracy(((Emin_m - Emin_mm) + mu*(m_value - mm_value))/kT)
-          exp_expr = _accuracy(np.exp(exp_expr_pow))
-          
-          # Nm/Nmm
-          attempts_cnt = experiment_cnts[m_idx] / experiment_cnts[mm_index] 
-          
-          # Pmm/Pm
-          permutation = _accuracy(calc_permutation(m_value, mm_value, _accuracy))
-
-          sum_botton += exp_expr * attempts_cnt * permutation * delta_E_sums[mm_index]
-        
-        Wm_value = sum_top / sum_botton
-        
-        Wm_array[m_idx, t_i, mu_i] = Wm_value
-        
-        mu_i += 1
-      
-  return Wm_array
   
